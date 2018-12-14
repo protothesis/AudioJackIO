14b759753fab9b293fd835896816b2c987c9b1e9
protothesis <33342229+protothesis@users.noreply.github.com>
2018-12-14 17:42:42 -0600

    Update code.py
    added a second output jack...
    started to try to solve the 'connection' signature

--- code.py
+++ code.py
@@ -11,8 +11,12 @@ led = DigitalInOut(board.D13)
 led.direction = Direction.OUTPUT
 
 # Output Jack
-outjack = DigitalInOut(board.D3)
-outjack.direction = Direction.OUTPUT
+outjack01 = DigitalInOut(board.D3)
+outjack01.direction = Direction.OUTPUT
+
+# Output Jack 02
+outjack02 = DigitalInOut(board.D4)
+outjack02.direction = Direction.OUTPUT
 
 # Input Jack
 injack = DigitalInOut(board.D8)
@@ -32,9 +36,11 @@ def isItTime(previous_time, pause_duration):  # returns if its time to do a thin
 	#
 	return currentTime() - previous_time > pause_duration
 
-def outJackIsOn():
-	#
-	return not outjack.value
+# this is a solution for checking multiple jacks...
+# but im not currently using it...
+# def outJackIsOn(outjack):  # make sure to pass in which outjack you want
+# 	#
+# 	return not outjack.value
 
 def inJackIsOn():
 	#
@@ -49,8 +55,14 @@ def inJackIsOn():
 
 
 ### //// NEW STATE VARIABLES (standard python underscore separator)
-pause_duration_output = .5
-previous_time_toggle_output = currentTime()
+pause_duration_outjack01 = .5
+previous_time_toggle_outjack01 = currentTime()
+
+pause_duration_outjack02 = .1
+previous_time_toggle_outjack02 = currentTime()
+
+# CONNECTOR START
+# connection_state = 
 
 # _output = outjack.value
 # _input = injack.value  
@@ -58,22 +70,29 @@ previous_time_toggle_output = currentTime()
 
 print("The Device has Initialized...")
 
+# CONNECTOR START
+input_stream_buffer = []
+# there needs to be a buffer to check the output signals against
+# need a sort of "clock" track (ie 1111...) before the signal itself 
+# to know when to check the buffer for the signal itself...
+# quite an involved problem afterall...
+
 
 while True:
 
-	if isItTime(previous_time_toggle_output, pause_duration_output):
-		# _output = not _output
-		# using the _output and _input variables in this way didnt work...
+	if isItTime(previous_time_toggle_outjack01, pause_duration_outjack01):
+		outjack01.value = not outjack01.value
+		previous_time_toggle_outjack01 = currentTime()
 
-		outjack.value = not outjack.value
-		previous_time_toggle_output = currentTime()
+	if isItTime(previous_time_toggle_outjack02, pause_duration_outjack02):
+		outjack02.value = not outjack02.value
+		previous_time_toggle_outjack02 = currentTime()
 
-		# print("")
-		# print("isItTime checked...")
-		# print("output jack = ", outJackIsOn())
-		# print("input jack = ", inJackIsOn())
 
 	# set led equal to injack
 	led.value = inJackIsOn()
+
+	# CONNECTOR START
+	# print("connection +", connection_state)
 
 	time.sleep(0.01)  # make bigger to slow down
